9c4c4eb4d8b901ad97e2e24e3d6ec58685bac26f
real_sohel <salmanisohel26@gmail.com>
2025-08-27 18:41:35 +0530

    Detect-a-cycle-in-a-linked-list

---
+++ Linked-List/Medium-LL/4-Detect-a-cycle-in-a-linked-list.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* fast=head;
+        ListNode* slow=head;
+
+        while(fast and fast->next){
+            fast=fast->next->next;
+            slow=slow->next;
+
+            if(fast==slow){
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
