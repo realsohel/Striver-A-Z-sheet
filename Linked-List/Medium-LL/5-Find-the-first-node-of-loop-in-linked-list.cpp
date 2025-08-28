d43a180ed45fd04cc24bb36b184efdb59029bc52
real_sohel <salmanisohel26@gmail.com>
2025-08-28 19:19:48 +0530

    Find-the-first-node-of-loop-in-linked-list

---
+++ Linked-List/Medium-LL/5-Find-the-first-node-of-loop-in-linked-list.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+
+        while(fast && fast->next){
+            fast=fast->next->next;
+            slow=slow->next;
+
+            if(fast==slow){
+                slow=head;
+
+                while(slow!=fast){
+                    slow=slow->next;
+                    fast=fast->next;
+                }
+
+                return slow;
+            }
+            
+        }
+        
+        return NULL;
+    }
+
+
+    // Brute Force 
+    // Time = O(N*LogN)
+    // Space = O(N)
+    ListNode *detectCycle(ListNode *head) {
+        
+        ListNode *temp=head;
+        unordered_map<ListNode*,int> mp;
+
+        while(temp){
+            mp[temp]++;
+
+            if(mp[temp]>1)
+                return temp;
+            
+            temp=temp->next;
+        }
+
+        return NULL;
+    }
+
+
+};
