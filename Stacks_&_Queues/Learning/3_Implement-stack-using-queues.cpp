f7042215e307e603628bc4c54619af9b84247a7e
real_sohel <salmanisohel26@gmail.com>
2025-10-29 15:56:31 +0530

    Solved implementation of Stack and Queue using Queue and stack respectively.

---
+++ Stacks_&_Queues/Learning/3_Implement-stack-using-queues.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class MyStack {
+public:
+    queue<int> q1;
+    queue<int> q2;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q2.push(x);
+
+        while(!q1.empty()){
+            q2.push(q1.front());
+            q1.pop();
+        }
+        while(!q2.empty()){
+            q1.push(q2.front());
+            q2.pop();
+        }
+
+    }
+    
+    int pop() {
+        if(q1.empty())
+            return -1;
+        
+        int p = q1.front();
+        q1.pop();
+        return p;
+    }
+    
+    int top() {
+        return q1.front();
+    }
+    
+    bool empty() {
+        return q1.size()==0;
+    }
+};
