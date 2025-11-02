ee988a023b7885f85473bc161821ff1434ad6275
real_sohel <salmanisohel26@gmail.com>
2025-11-02 11:30:07 +0530

    Implemented-min-stack

---
+++ Stacks_&_Queues/Learning/8_Implement-min-stack.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class MinStack {
+public:
+    stack<pair<int,int>> stk;
+    int minVal=0;
+    MinStack() {    
+        minVal=INT_MIN;
+    }
+    
+    void push(int val) {
+        if(stk.empty())
+            minVal=val;
+        
+        minVal=min(minVal,val);
+        stk.push({val,minVal});
+    }
+    
+    void pop() {    
+        if(stk.empty())
+            return;
+        stk.pop();
+        
+        if (!stk.empty())
+            minVal = stk.top().second;
+        else
+            minVal = INT_MAX;
+    }
+    
+    int top() {
+        if(stk.empty())
+            return -1;
+        return stk.top().first;
+    }
+    
+    int getMin() {
+        if(stk.empty())
+            return -1;
+        return stk.top().second;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
